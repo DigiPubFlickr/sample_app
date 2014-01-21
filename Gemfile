d52c7e83077fa8048f963fbc7a3471f85fb89de8
Wang <DigiPub69@gmail.com>
2014-01-21 20:54:37 +0900

    Add a full_title helper

--- Gemfile
+++ Gemfile
@@ -3,10 +3,12 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
 
+#gem 'pg', '0.15.1'
 group :development do
 # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
   gem 'rspec-rails', '2.13.1'
+  gem 'guard-rspec', '2.5.0'
 end
 
 group :test do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -30,26 +30,39 @@
  require 'spec_helper'
  
  describe "Static pages" do
-   #integrate_views
-   #For Rspec2, replace integrate_views with render_views
-   #render_views
-   #https://www.ruby-forum.com/topic/139941#621914 
-   #integrate_views
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
  
-   describe "Home page" do
+  #describe "Home page" do
  
-     it "should have the h1 'Sample App'" do
-       visit '/static_pages/home'
-       page.should have_selector('h1', :text => 'Sample App')
-     end
+  #  it "should have the h1 'Sample App'" do
+  #    visit '/static_pages/home'
+  #    page.should have_selector('h1', :text => 'Sample App')
+  #  end
  
-     it "should have the title 'Home'" do
-       visit '/static_pages/home'
-       #page.should have_selector('title',
-       #                  :text => "Ruby on Rails Tutorial Sample App | Home")
-       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-     end
-   end
+  #  it "should have the title 'Home'" do
+  #    visit '/static_pages/home'
+  #    expect(page).to have_title("#{base_title} | Home")
+  #  end
+  #end
+
+    describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
+  end
 
   describe "Help page" do
 
@@ -62,7 +75,7 @@
       visit '/static_pages/help'
       #page.should have_selector('title',
       #                  :text => "Ruby on Rails Tutorial Sample App | Help")
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -74,15 +87,21 @@
     end
 
     it "should have the title 'About Us'" do
-      #integrate_views
       visit '/static_pages/about'
-      #page.should have_selector('title',
-      #              :text => "Ruby on Rails Tutorial Sample App | About Us")
-      #page.should have_content("About Us")
-      #page.should have_css('title', :text => 'Ruby on Rails Tutorial Sample App | About Us')
-      #response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About Us")
-      #response.should have_selector("title:contains('Ruby on Rails Tutorial Sample App | About Us')")
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
