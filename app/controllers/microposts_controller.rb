f591a8d94bee6803ee6c5803ef0e03e5b5f4b620
DigiPubFlickr <DigiPub69@gmail.com>
2014-01-28 02:23:33 +0900

    Update microposts_controller.rb
        def micropost_params
          params.require(:micropost).permit(:content)
        end

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -19,7 +19,11 @@ class MicropostsController < ApplicationController
   end
 
   private
-
+  
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
+    
     def correct_user
       @micropost = current_user.microposts.find_by_id(params[:id])
       redirect_to root_url if @micropost.nil?
